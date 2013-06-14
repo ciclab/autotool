bbf82f48cd437089dadad2ffef5c14c3fd7501a7
shenjiaqi <milkpowder2008@yeah.net>
2013-06-15 00:18:04 +0800

    完善展开函数 修改输入文件
    目前只能对instructuion,type和enum展开。发现register也要展开，明天再写

--- src/simulator/read.cpp
+++ src/simulator/read.cpp
@@ -183,19 +183,21 @@ string get_full_name(string name,string pwd)
 //   assert(asem.size()==2);
 //   string get_full_name(asem.ivec[1].name,pwd);
 // }
-bool is_instr(const string &name)
+bool is_instr(string name)
 {
   return (hc.find((string)"...instruction."+name)!=0);
 }
-bool is_type(const string &name)
+bool is_type(string name)
 {
   return (hc.find((string)"...type."+name)!=0);
 }
-bool is_enum(const string &name)
+bool is_enum(string name)
 {
   return (hc.find((string)"...enum."+name)!=0);
 }
-Asem *get_asem(const string &name)
+// 返回name对应的那一项asem地址，现在的实现是首先假设是instruction，
+// 然后是type最后是enum。对这些可能依次查找
+Asem *get_asem(string name)
 {
   int tmp=hc.find((string)"...instruction."+name);
   if(tmp==0)
@@ -218,6 +220,8 @@ int unfold_type(const Asem &asem)
   cout<<asem.ivec[0].name<<endl;
   return 0;
 }
+
+// 每个变量已经定好取值后计算code，binary，do并保存到r中
 void eval_unfold(vector<string> &var_name,
 		 vector<vector<string> >&var_choose_name,
 		 vector<vector<int> >&var_val,
@@ -231,10 +235,14 @@ void eval_unfold(vector<string> &var_name,
   vector<int> var_off_in_code;
   // 记录变量在binary中出现的位置，目前认为一个变量在binary中只出现一次
   vector<int> var_off_in_bin;
+
   var_off_in_code.resize(var_name.size());
   var_off_in_bin.resize(var_name.size());
+
+  // r中新增一项保留结果
   int k=r.size();
   r.resize(k+1);
+
   // 对code和binary的求值应该可以写成函数
   // 求出code
   assert(code.ivec[0].name==(string)"code");
@@ -251,8 +259,10 @@ void eval_unfold(vector<string> &var_name,
 	  // 找到对应的变量在var_list的索引，目前是顺序查找
 	  for(;j<var_name.size() && var_name[j]!=code.ivec[i].name;++j)
 	    ;
-	  var_off_in_code[j]=r[k].code.length();
 	  assert(j<var_name.size());
+
+	  // 记录找到的变量在code中的偏移
+	  var_off_in_code[j]=r[k].code.length();
 	  r[k].code+=unfolded_list[var_val[j][var_choosed_val[j].first]][var_choosed_val[j].second].code;
 	}
       else assert(0);
@@ -270,6 +280,7 @@ void eval_unfold(vector<string> &var_name,
 	  for(;j<var_name.size() && var_name[j]!=binary.ivec[i].name;++j)
 	    ;
 	  assert(j<var_name.size());
+
 	  var_off_in_bin[j]=r[k].binary.length();
 	  r[k].binary+=unfolded_list[var_val[j][var_choosed_val[j].first]][var_choosed_val[j].second].binary;
 	}
@@ -277,43 +288,50 @@ void eval_unfold(vector<string> &var_name,
 	{
 	  // 目前只可能是switch语句
 	  assert(binary.ivec[i].ivec[0].name==(string)"switch");
+	  
+	  // 找到switch中指定的变量在var_name中的序号
 	  int j=0;
 	  for(;j<var_name.size() && var_name[j]!=binary.ivec[i].ivec[1].name;++j)
 	    ;
 	  assert(j<var_name.size());
-	  int k=2;
-	  // 遍历switch中每一个选项
-	  for(;k<binary.ivec[i].ivec.size() &&
-		var_choose_name[j][var_choosed_val[j].first]!=binary.ivec[i].ivec[k].name;k+=2)
-	    ;
-	  assert(k<binary.ivec[i].ivec.size());
+
+	  int z=2;
+	  // 遍历switch中每一个选项，找到当前匹配的那一项
+	  for(;z<binary.ivec[i].ivec.size() &&
+		var_choose_name[j][var_choosed_val[j].first]!=binary.ivec[i].ivec[z].name;z+=2)
+	    assert(binary.ivec[i].ivec[z].type==type_is_string1);
+	  assert(z+1<binary.ivec[i].ivec.size());
+
 	  // 现在认为switch后面的值只是常量string，以后应该处理其他情况
-	  assert(binary.ivec[i].ivec[k+1].type==type_is_string2);
-	  r[k].binary+=((binary.ivec[i]).ivec[k+1]).name;
+	  assert(binary.ivec[i].ivec[z+1].type==type_is_string2);
+	  r[k].binary+=binary.ivec[i].ivec[z+1].name;
 	}
     }
   // 计算新生成的code offset和binary offset 信息
   for(int i=0;i<var_val.size();++i)
-    for(int j=0;j<
-	  unfolded_list[var_val[i][var_choosed_val[i].first]][var_choosed_val[i].second].off_in_code.size();++j)
-      {
-	r[k].off_in_code.push_back(unfolded_list[var_val[i][var_choosed_val[i].first]][var_choosed_val[i].second].off_in_code[j]+
-				   var_off_in_code[i]);
-	r[k].off_in_binary.push_back(unfolded_list[var_val[i][var_choosed_val[i].first]][var_choosed_val[i].second].off_in_binary[j]+
-				     var_off_in_bin[i]);
-      }
+    {
+      int a=var_val[i][var_choosed_val[i].first];
+      int b=var_choosed_val[i].second;
+      int oc=var_off_in_code[i];
+      int ob=var_off_in_bin[i];
+      for(int j=0;j<unfolded_list[a][b].off_in_code.size();++j)
+	{
+	  r[k].off_in_code.push_back(unfolded_list[a][b].off_in_code[j]+oc);
+	  r[k].off_in_binary.push_back(unfolded_list[a][b].off_in_binary[j]+ob);
+	}
+    }
   // 处理do描述
       //for(int i=1;i<
 }
-void dfs_unfold_instr(vector<string> &var_name,
-		      vector<vector<string> > &var_choose_name,
-		      vector<vector<int> > &var_val,
-		      vector<pair<int,int> > &var_choosed_val,
-		      Asem &code,
-		      Asem &binary,
-		      Asem &doo,
-		      int lev,
-		      vector<triple> &r)
+void dfs_unfold_instr(vector<string> &var_name, // 变量名字
+		      vector<vector<string> > &var_choose_name, // 变量可选的值的名字
+		      vector<vector<int> > &var_val,		// 变量可选值对应unfolded_list的索引
+		      vector<pair<int,int> > &var_choosed_val,	// 变量取值的情况
+		      Asem &code,				// 待展开的code对应asem
+		      Asem &binary,				// 待展开的binary对应asem
+		      Asem &doo,				// 待展开do对应的asem
+		      int lev,					// 当前枚举到第lev个变量
+		      vector<triple> &r)			// 保存展开结果
 {
   if(lev>=var_name.size())
     eval_unfold(var_name,var_choose_name,var_val,var_choosed_val,
@@ -321,21 +339,19 @@ void dfs_unfold_instr(vector<string> &var_name,
   else
     for(int i=0;i<var_val[lev].size();++i)
       {
+	// 第lev个变量选择第i个值
 	var_choosed_val[lev].first=i;
 	for(int j=0;j<unfolded_list[var_val[lev][i]].size();++j)
 	  {
+	    // 第i个值中第j个展开项
 	    var_choosed_val[lev].second=j;
 	    dfs_unfold_instr(var_name,var_choose_name,var_val,var_choosed_val,
 			     code,binary,doo,lev+1,r);
 	  }
       }
 }
-// 在unfolded_list的第i项里增加一个triple
-void new_unfolded_triple(int i,string code,string binary,string doo)
-{
-  unfolded_list[i].push_back(triple(code,binary,doo));
-}
-// 返回v的二进制，宽度是w
+
+// 返回v的二进制，宽度能表示w项（从0开始）
 string num2string(int v,int w)
 {
   string r;
@@ -353,19 +369,27 @@ string num2string(int v,int w)
 // 它的位置决定
 int unfold_enum(Asem &asem)
 {
+  // 新增一项
   int k=unfolded_list.size();
   unfolded_list.resize(k+1);
+
   for(int i=1;i<asem.ivec.size();++i)
     {
       if(asem.ivec[i].type==type_is_vector)
 	{
-	  // 如果是ivec那么只有两个元素
+	  // 如果是ivec那么只有两个元素,且都是有引号的string
 	  assert(asem.ivec[i].ivec.size()==2 && 
-		 asem.ivec[i].ivec[0].type==type_is_vector && 
-		 asem.ivec[i].ivec[1].type==type_is_vector);
-	  new_unfolded_triple(k,asem.ivec[i].ivec[1].name,num2string(i-1,asem.ivec.size()-1),(string)"");
+		 asem.ivec[i].ivec[0].type==type_is_string2 && 
+		 asem.ivec[i].ivec[1].type==type_is_string2);
+	  unfolded_list[k].push_back(triple(asem.ivec[i].ivec[1].name,num2string(i-1,asem.ivec.size()-1),(string)""));
+	}
+      else
+	{
+	  assert(asem.ivec[i].type==type_is_string2);
+	  unfolded_list[k].push_back(triple(asem.ivec[i].name,num2string(i-1,asem.ivec.size()-1),(string)""));
 	}
     }
+  return k;
 }
 // 把string转成int
 int string2num(string s)
@@ -378,45 +402,61 @@ int string2num(string s)
 }
 // 处理type描述
 // (name (width num) (flag xxx) )
+// 生成的code是"name_width_xxx"
+// 生成的binary是"-..."(共width个"-")
 int unfold_type(Asem &asem)
 {
+  // type定义有3个部分
   assert(asem.ivec.size()==3);
+
   Asem & tmpw=*asem.find("width");
   assert(tmpw.ivec.size()==2 && tmpw.ivec[1].type==type_is_string1);
   int width=string2num(tmpw.ivec[1].name);
+
   Asem & tmpf=*asem.find("flag");
   assert(tmpf.ivec.size()==2 && tmpf.ivec[1].type==type_is_string1);
+
   int k=unfolded_list.size();
   unfolded_list.resize(k+1);
+
   string binary;
   for(int i=0;i<width;++i)
     binary+=(string)"-";
-  new_unfolded_triple(k,(string)"type_"+asem.ivec[0].name+(string)"_"+
-		     tmpw.ivec[1].name+(string)"_"+tmpf.ivec[1].name,
-		     binary,(string)"");
+  unfolded_list[k].push_back(triple((string)"type_"+asem.ivec[0].name+
+				    (string)"_"+tmpw.ivec[1].name+
+				    (string)"_"+tmpf.ivec[1].name,
+				    binary,(string)""));
+  unfolded_list[k][0].off_in_code.push_back(0);
+  unfolded_list[k][0].off_in_binary.push_back(0);
+  return k;
 }
 // 对instruction展开
 int unfold_instr(Asem &asem)
 {
   // 变量名
   vector<string> var_name;
-  // 变量可选的值
+  // 变量可选的值(可能是其它instruction或者是type或者是enum)
   vector<vector<string> >var_choose_name;
-  // 变量选不同值对应的索引
+  // 变量选不同值对应的在unfolded_list中的索引
   vector<vector<int> > var_val;
 
-  // 找到所有变量定义 这里认为是这个样子的：（= a b c ...）
-  // 且假设变量定义很简单 没有嵌套 递归
-  // 所有变量定义在最外面一层
+  // 找到所有变量定义 这里认为是这个样子的：
+  // （= a b c ...）
+  // 表示a可以是b或c或...
+  // 假设变量定义很简单,没有嵌套和递归
+  // 所有变量定义在当前instruction描述的最外面一层
   for(int i=1;i<asem.ivec.size();++i)
     if(asem.ivec[i].ivec[0].name==(string)"=")
       {// 这一项是变量定义
+	// 这是第k个变量
 	int k=var_name.size();
 	var_name.resize(k+1);
 	var_choose_name.resize(k+1);
+	var_val.resize(k+1);
+
 	// 保存变量名a
 	var_name[k]=asem.ivec[i].ivec[1].name;
-	// j从2开始 第一个可选的值
+	// j从2开始,是第一个可选的值
 	for(int j=2;j<asem.ivec[i].ivec.size();++j)
 	  {
 	    const string &name=asem.ivec[i].ivec[j].name;
@@ -436,27 +476,40 @@ int unfold_instr(Asem &asem)
 	  }
       }
 
-  // 保存待展开instruction项的binary,code,do
+  // 保存待展开instruction项的binary,code,do的asem地址
   Asem & code=*asem.find("code");
   Asem & binary=*asem.find("binary");
   Asem & doo=*asem.find("do");
-
   assert((&code!=0) && (&binary!=0) && (&doo!=0));
+
+  // 在unfolded_list新增加一项
   int k=unfolded_list.size();
   unfolded_list.resize(k+1);
+
+  // 保存当前变量取的值，二元组表示：
+  // (= a b c)
+  // 第一项表示a取b还是c
+  // 第二项表示取b或是c中的第几条（b和c已被展开，可能有多条可选项）
   vector<pair<int,int> > var_choosed_val;
+  var_choosed_val.resize(var_name.size());
+  // 针对变量取不同值的时候得到展开的code，binary和do
   dfs_unfold_instr(var_name,var_choose_name,var_val,var_choosed_val,
 		   code,binary,doo,0,unfolded_list[k]);
+  return k;
 }
 
 // 展开一个描述，包括枚举，指令，类型
 int unfold(Asem &asem)
 {
   string & name=asem.ivec[0].name;
-  cout<<'!'<<name<<endl;
+
   // 如果已经展开过
   if(hc_unfold.find(name)!=0)
-    return hc_unfold.find(name)-1; // 由于加入hash表的时候加过1
+    {
+      return hc_unfold.find(name)-1; // 由于加入hash表的时候加过1
+    }
+
+  cout<<"unfolding:"<<name<<endl;
 
   int r;
   // 对不同的类型分别展开
@@ -467,26 +520,26 @@ int unfold(Asem &asem)
   else if(is_type(name))
     r=unfold_type(asem);
   else assert(0);
-
+  cout<<"done:"<<name<<endl;
   // 加入hash表，由于r可能是0，而hash.find返回0表示没有找到
   hc_unfold.insert(name,r+1);
-
   return r;
 }
 
 int main(){
   freopen("in.txt","r",stdin);
   Asem asem;
-  asem.type=type_is_vector;
-  gen(asem);
-  //display(asem,0);
 
-  // 保留字
+  asem.type=type_is_vector;
+  // 读入文件
+  gen(asem);
+  // display(asem,0);
+
+  // 把保留字加入hash表
   for(int i=0;i<sizeof(reserved)/sizeof(reserved[0]);++i)
     hc.insert(reserved[i].c_str(),(int)(void*)reserved);
   
   // 插入hash表
-  
   dfs_insert_hash(asem,"");
 
   // instruction里必须有顶层规则
