16ede127cb4d6ede62f67941f4574cbe56b08407
shenjiaqi <ciclab@yeah.net>
2013-09-25 20:56:56 +0800

    add def.h file to git

---
+++ src/include/def.h
@@ -0,0 +1,9 @@
+#ifndef _DEF_H_
+#define _DEF_H_
+#include <utility>
+#include <vector>
+using namespace std;
+#define FOR(a,b,c) for(typeof(b) a=b;a<c;++a)
+#define FR(a,b) for(typeof(b.begin()) a=b.begin();a!=b.end();++a)
+typedef pair<int,int> ppi;
+#endif
