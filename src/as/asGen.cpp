165c1d398ee0a940f1f13b841250ee9e02f7b5fb
shenjiaqi <ciclab@yeah.net>
2013-09-25 20:52:41 +0800

    generating binary-code

--- src/as/asGen.cpp
+++ src/as/asGen.cpp
@@ -14,20 +14,37 @@ int main(int argc,char *argv[])
   
   int instr_size=ir.get_instr_size();
   string top_rule_name=ir.get_top_rule_name();
-  
+  ofstream fout;
+  fout.open(argv[2],ofstream::out);
   FOR(i,0,instr_size)
     {
       string n=ir.get_instr_name(i);
       if(n.compare(0,top_rule_name.length(),top_rule_name)==0)
 	{
-	  cout<<n<<endl;
-	  cout<<ir.get_instr_code(i)<<endl;
-	  cout<<ir.get_instr_binary(i)<<endl;
+#if ASGEN_COUT==1
+	  cout<<<<endl;
 	  vector<ppi> off;
 	  ir.get_instr_off(i,off);
 	  FR(i,off)
 	    cout<<i->first<<' '<<i->second<<endl;
 	  cout<<endl<<endl;
+#endif
+	  string code=ir.get_instr_code(i);
+	  string binary=ir.get_instr_binary(i);
+	  fout<<"\t\\\\"<<n<<' '<<binary<<endl;
+	  FR(i,binary)
+	    switch(*i)
+	      {
+	      case '1':
+		fout<<"C_1 ";break;
+	      case '0':
+		fout<<"C_0 ";break;
+	      case '-':
+	      case 'x':
+		fout<<"C_0_1 ";break;
+	      default: assert(0);
+	      }
+	  fout<<endl;
 	}
     }
   return 0;
