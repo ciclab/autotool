1130da0899c78f6ea6af1baa1ee9d77c82320a55
shenjiaqi <ciclab@yeah.net>
2013-09-26 21:32:08 +0800

    fix some bug in input

--- src/as/asGen.cpp
+++ src/as/asGen.cpp
@@ -24,7 +24,7 @@ int main(int argc,char *argv[])
   fout<<"BLANK [ \\t]+\n";
 
   fout<<"%%\n";
-
+  //output rules for 
   fout<<"BLANK {}\n";
   //fout<<"\\n return 0;\n";
 
@@ -42,66 +42,46 @@ int main(int argc,char *argv[])
 	  ir.get_instr_off(i,off);
 	  string code=ir.get_instr_code(i);
 	  string binary=ir.get_instr_binary(i);
-	  //int off_in_code=0,offi=0;
+	  int off_in_code=0,offi=0;
 	  sort(off.begin(),off.end());
-	  FR(ii,off)
-	    cout<<ii->first<<' ';
-	  cout<<endl;
-	  FR(jj,code)
-	    if(*jj==c_sep || *jj==c_type_beg || *jj==c_enum_beg)
-	      cout<<'!';
-	    else cout<<*jj;
-	  cout<<endl;
-	  FR(jj,code)
+	  //fout<<off.size()<<' '<<n<<endl;
+	  for(typeof(code.begin()) j=code.begin();j!=code.end();)
 	    {
-	      if(*jj==c_sep)
-		cout<<' ';
-	      else if(*jj==c_type_beg)
-		cout<<' '<<'t'<<' ';
-	      else if(*jj==c_enum_beg)
-		cout<<' '<<'e'<<' ';
-	      else cout<<*jj;
+	      switch(*j)
+		{
+		case ' ':
+		  for(;j!=code.end() && *j==' ';++j,++off_in_code)
+		    ;
+		  fout<<"BLANK";
+		  break;
+		case c_sep:
+		  ++j,++off_in_code;
+		default:
+		  string token;
+		  for(;offi<(int)off.size() && off[offi].first<off_in_code;++offi)
+		    ;
+		  bool is_string=true;
+		  if(offi<(int)off.size() && off[offi].first==off_in_code)
+		    {
+		      //may be a type or enum;
+		      is_string=false;
+		    }
+		  if(!is_string)
+		    {
+		      assert(*j==c_type_beg || *j==c_enum_beg);
+		      ++off_in_code;
+		      ++j;
+		    }
+		  for(;j!=code.end() && *j!=' ' && *j!='\1';++j,++off_in_code)
+		    token+=*j;
+		  if(is_string)
+		    fout<<'"'<<token<<'"';
+		  else fout<<'{'<<token<<'}';
+		  break;
+		}
 	    }
-	  cout<<endl;
-	//   for(typeof(code.begin()) j=code.begin();j!=code.end();)
-	//     {
-	//       switch(*j)
-	// 	{
-	// 	case ' ':
-	// 	  for(;j!=code.end() && *j==' ';++j,++off_in_code)
-	// 	    ;
-	// 	  fout<<"BLANK";
-	// 	  break;
-	// 	case c_sep:
-	// 	  ++j,++off_in_code;
-	// 	default:
-	// 	  string token;
-	// 	  for(;offi<(int)off.size() && off[offi].first<off_in_code;++offi)
-	// 	    ;
-	// 	  bool is_string=true;
-	// 	  if(offi<(int)off.size() && off[offi].first==off_in_code)
-	// 	    {
-	// 	      //may be a type or enum;
-	// 	      //fout<<"##";
-	// 	      is_string=false;
-	// 	    }
-	// 	  if(!is_string)
-	// 	    {
-	// 	      FR(ii,off)
-	// 		cout<<ii->first<<' ';cout<<endl;
-	// 	      cout<<code<<' '<<*j<<' '<<(int)*j<<' '<<(j-code.begin())<<endl;
-	// 	      assert(*j==c_type_beg || *j==c_enum_beg);
-	// 	      ++j;
-	// 	    }
-	// 	  for(;j!=code.end() && *j!=' ' && *j!='\1';++j,++off_in_code)
-	// 	    token+=*j;
-	// 	  if(is_string)
-	// 	    fout<<'"'<<token<<'"';
-	// 	  else fout<<' '<<token;
-	// 	  break;
-	// 	}
-	//     }
-	//   fout<<endl;
+	  fout<<"{return \""<<binary<<"\";}"<<endl;
+	  fout<<endl;
 	}
     }
   return 0;
